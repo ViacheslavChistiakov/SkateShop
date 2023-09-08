7b41f5ebf05a9bbddbc1b0942a5039dd4155506c
Vyacheslav <chystiakov.vycheslav@gmail.com>
2023-09-09 01:39:13 +0300

    fixed litle-bit html

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import Categories from './pages/Categories.tsx';
 import Product from './pages/Product.tsx';
 import Filter from './pages/Filter.tsx';
 import Sort from './pages/Sort.tsx';
+import board1 from  './assets/board1.png';
+import board2 from  './assets/board2.png';
+import board3 from  './assets/board3.png';
 import React from 'react';
 
 
@@ -17,13 +20,24 @@ import React from 'react';
 
 function App() {
 
+const [items, setItems] = React.useState([]);
+
+fetch('http://localhost:3001/Decks').then((res) => {
+ return res.json();
+}).then(json => {
+  console.log(json);
+})
 
   return (
     <div className="App">
       <Header />
         <SectionFirst />
           <Categories />
-        <Product />
+          <div className="product">
+            <Product  title='Stefan Janoski Sabo' size='8.25' image={board1} price='59$' />
+            <Product  title='MONKEY FROM JUNGLE' size='8.75' image={board2} price='67$' />
+            <Product  title='SOUR No Woman No Cry' size='8.25' image={board3} price='89$' />
+            </div>
           <Filter />
           <Sort />
 
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -3,21 +3,26 @@ import board1 from  '../assets/board1.png';
 import board2 from  '../assets/board2.png';
 import board3 from  '../assets/board3.png';
 
+interface props {
+  title: string,
+  size: string,
+  image: any,
+  price: string
+}
 
 
-function Product () {
-
+function Product (props: props) {
 
 
 
     return (
-        <div className="product">
-        <div className="first-item">
-          <img className='product-view' src={board1} alt="" />
+
+        <div  className="first-item">
+          <img className='product-view' src={props.image} alt="" />
           <div className="product-info">
-          <h1 className="title-product">Stefan Janoski Sabor</h1>
+          <h1 className="title-product">{props.title}</h1>
             <ul className='product-about'>
-              <li className='size'>Size: 8.25</li>
+              <li   className='size'>Size: {props.size}</li>
               <li className='collection'>Collection: 2023</li>
             </ul>
             <p className='product-info_wrapper'>
@@ -25,45 +30,9 @@ function Product () {
             </p>
           </div>
           <div className="product-price">
-              <h1 className='price_inner'>59$</h1>
+              <h1 className='price_inner'>{props.price}</h1>
               <button className='buy_item'>BUY</button>
           </div>
-        </div>
-        <div className="first-item">
-          <img className='product-view' src={board2} alt="" />
-          <div className="product-info">
-          <h1 className="title-product">ALIEN WORKSHOP x Thrasher Missing</h1>
-              <ul className='product-about'>
-                <li className='size'>Size: 8.75</li>
-                <li className='collection'>Collection: 2023</li>
-            </ul>
-            <p className='product-info_wrapper'>
-            7 layers 100% of Canadian Maple
-            </p>
-          </div>
-          <div className="product-price">
-              <h1 className='price_inner'>67$</h1>
-              <button className='buy_item'>BUY</button>
-          </div>
-        </div>
-        <div className="first-item">
-          <img className='product-view' src={board3} alt="" />
-          <div className="product-info">
-          <h1 className="title-product">SOUR No Woman No Cry</h1>
-            <ul className='product-about'>
-                <li className='size'>Size: 8.25</li>
-                <li className='collection'>Collection: 2023</li>
-            </ul>
-            <p className='product-info_wrapper'>
-            7 layers 100% of Canadian Maple
-            </p>
-          </div>
-          <div className="product-price">
-              <h1 className='price_inner'>89$</h1>
-              <button className='buy_item'>BUY</button>
-          </div>
-        </div>
-
       </div>
     )
 }
